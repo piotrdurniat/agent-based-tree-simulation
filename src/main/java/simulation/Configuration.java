6f3f16cb7506fc062aa7319cc228435309950a32
Piotr Durniat <piotr@durniat.com>
2020-05-23 21:58:02 +0200

    Simplified the way to access configuration values

--- src/main/java/simulation/Configuration.java
+++ src/main/java/simulation/Configuration.java
@@ -9,13 +9,20 @@ public class Configuration {
     static private String filePath = "src/simulation.config";
     static private InputStream inputStream;
 
+    public static int maxIterations;
+    public static float leafMinLight;
+
     static void loadConfig() {
         try {
             inputStream = new FileInputStream(filePath);
             properties.load(inputStream);
+
+            maxIterations = Integer.parseInt(getProperty("maxIterations"));
+
         } catch (Exception exception) {
             System.out.println(exception);
         }
+
     }
 
     public static String getProperty(String propertyName) {
--- src/main/java/simulation/IntervalTasks.java
+++ src/main/java/simulation/IntervalTasks.java
@@ -11,27 +11,22 @@ public class IntervalTasks extends TimerTask {
     public static int turnNum;
     public static Tree tree;
     public static Timer timer;
-    public static int maxIterations;
 
     /**
      * @param tree Tree object passed to evaluate it's turns on every interval
      */
     public static void setup(Tree tree) {
-
         IntervalTasks.tree = tree;
-
         timer = new Timer();
         TimerTask task = new IntervalTasks();
         timer.schedule(task, 200, 16);
-
-        maxIterations = Integer.parseInt(Configuration.getProperty("maxIterations"));
     }
 
     /**
      * Interval function invoked after the set amount of time
      */
     public void run() {
-        if (turnNum >= maxIterations) {
+        if (turnNum >= Configuration.maxIterations) {
             timer.cancel();
         }
 
