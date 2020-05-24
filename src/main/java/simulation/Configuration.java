8724653ea6f72fabbd08b3495e0205697cb8f645
Piotr Durniat <piotr@durniat.com>
2020-05-24 15:26:17 +0200

    Organized configuration

--- src/main/java/simulation/Configuration.java
+++ src/main/java/simulation/Configuration.java
@@ -9,32 +9,36 @@ public class Configuration {
     private static String filePath = "src/simulation.config";
     private static InputStream inputStream;
 
+    // Properties related to simulation:
     public static int maxIterations;
-    public static float leafMinLight;
 
+    // Properties related to environment:
     public static float environmentLight;
     public static float environmentCO2;
     public static float environmentWater;
 
+    // Properties related to branch:
     public static float newBranchProbability;
     public static float newLeafProbability;
     public static float newFlowerProbability;
-
     public static float glucosePerBranch;
-
     public static int branchingFactor;
     public static float angleBetweenBranches;
 
+    // Properties related to leaf:
+    public static float leafMinLight;
     public static float maxLeafLength;
     public static float leafGrowthSpeed;
 
+    /**
+     * Reads configuration file and parses property values to variables
+     */
     static void loadConfig() {
         try {
             inputStream = new FileInputStream(filePath);
             properties.load(inputStream);
 
             maxIterations = Integer.parseInt(getProperty("maxIterations"));
-            leafMinLight = Float.parseFloat(getProperty("leafMinLight"));
 
             environmentLight = Float.parseFloat(getProperty("environmentLight"));
             environmentCO2 = Float.parseFloat(getProperty("environmentCO2"));
@@ -43,20 +47,25 @@ public class Configuration {
             newBranchProbability = Float.parseFloat(getProperty("newBranchProbability"));
             newLeafProbability = Float.parseFloat(getProperty("newLeafProbability"));
             newFlowerProbability = Float.parseFloat(getProperty("newFlowerProbability"));
-
             glucosePerBranch = Float.parseFloat(getProperty("glucosePerBranch"));
-
             branchingFactor = Integer.parseInt(getProperty("branchingFactor"));
             angleBetweenBranches = Float.parseFloat(getProperty("angleBetweenBranches"));
 
             maxLeafLength = Float.parseFloat(getProperty("maxLeafLength"));
             leafGrowthSpeed = Float.parseFloat(getProperty("leafGrowthSpeed"));
+            leafMinLight = Float.parseFloat(getProperty("leafMinLight"));
 
         } catch (Exception exception) {
             System.out.println(exception);
         }
     }
 
+    /**
+     * Returns property from configuration file with given name
+     * 
+     * @param propertyName Name of the property
+     * @return String The string value of property with given name
+     */
     public static String getProperty(String propertyName) {
         return properties.getProperty(propertyName);
     }
