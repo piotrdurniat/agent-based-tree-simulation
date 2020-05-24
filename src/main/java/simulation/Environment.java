9502c6cd368e5305d054a2c10a26cc2e5c0c1dc6
Piotr Durniat <piotr@durniat.com>
2020-05-24 10:59:37 +0200

    Added environment functions

--- src/main/java/simulation/Environment.java
+++ src/main/java/simulation/Environment.java
@@ -2,4 +2,19 @@ package simulation;
 
 public class Environment {
 
+    private static float light = Configuration.environmentLight;
+    private static float environmentC02 = Configuration.environmentC02;
+    private static float environmentWater = Configuration.environmentWater;
+
+    public float getLight() {
+        return light;
+    }
+
+    public float getCarbonDioxide() {
+        return environmentC02;
+    }
+
+    public float getWater() {
+        return environmentWater;
+    }
 }
