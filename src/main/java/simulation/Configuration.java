8ccc6a6017bb6262c7f7647460922d1424b80159
Piotr Durniat <piotr@durniat.com>
2020-05-24 10:58:53 +0200

    Added more config properties

--- src/main/java/simulation/Configuration.java
+++ src/main/java/simulation/Configuration.java
@@ -12,17 +12,39 @@ public class Configuration {
     public static int maxIterations;
     public static float leafMinLight;
 
+    public static float environmentLight;
+    public static float environmentC02;
+    public static float environmentWater;
+
+    public static float newBranchProbability;
+    public static float newLeafProbability;
+    public static float newFlowerProbability;
+
+    public static float maxBranchLength;
+    public static int branchingFactor;
+
     static void loadConfig() {
         try {
             inputStream = new FileInputStream(filePath);
             properties.load(inputStream);
 
             maxIterations = Integer.parseInt(getProperty("maxIterations"));
+            leafMinLight = Float.parseFloat(getProperty("leafMinLight"));
+
+            environmentLight = Float.parseFloat(getProperty("environmentLight"));
+            environmentC02 = Float.parseFloat(getProperty("environmentC02"));
+            environmentWater = Float.parseFloat(getProperty("environmentWater"));
+
+            newBranchProbability = Float.parseFloat(getProperty("newBranchProbability"));
+            newLeafProbability = Float.parseFloat(getProperty("newLeafProbability"));
+            newFlowerProbability = Float.parseFloat(getProperty("newFlowerProbability"));
+
+            maxBranchLength = Float.parseFloat(getProperty("maxBranchLength"));
+            branchingFactor = Integer.parseInt(getProperty("branchingFactor"));
 
         } catch (Exception exception) {
             System.out.println(exception);
         }
-
     }
 
     public static String getProperty(String propertyName) {
