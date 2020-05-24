c0aebba73950eee34c7a4df7003c4f9d39127bbe
Piotr Durniat <piotr@durniat.com>
2020-05-24 19:53:15 +0200

    Merged branch "develop-fruit-functionality" with "master"
    Squashed commit of the following:
    
    commit d6129de49c5bab1f5af6d19b1faf0f18015fb86a
    Author: Piotr Durniat <piotr@durniat.com>
    Date:   Sun May 24 19:35:54 2020 +0200
    
        Added fruit ripeness functionality
    
    commit 871b267f9f9439457b4adf3fe11339512abec740
    Author: Piotr Durniat <piotr@durniat.com>
    Date:   Sun May 24 17:42:27 2020 +0200
    
        Added functionality of turning a flower into a fruit
    
    commit 34fb0f2827f1486f9f5395700ad27e6ec5a15b2c
    Author: Piotr Durniat <piotr@durniat.com>
    Date:   Sun May 24 16:26:28 2020 +0200
    
        Added growing functionality to flower

--- src/main/java/simulation/tree/tree_organ/branch_component/subcomponents/Fruit.java
+++ src/main/java/simulation/tree/tree_organ/branch_component/subcomponents/Fruit.java
@@ -1,20 +1,48 @@
 package simulation.tree.tree_organ.branch_component.subcomponents;
 
+import simulation.Configuration;
 import simulation.tree.tree_organ.branch_component.Branch;
 
 public class Fruit extends BranchSubcomponent {
+
+    private float ripeness;
+    private static float ripeningSpeed = Configuration.fruitRipeningSpeed;
+
     public Fruit(Branch parentBranch) {
         super(parentBranch);
 
         graphics = new FruitGraphics(this);
+        ripeness = 0;
     }
 
     @Override
     public void evaluateTurn() {
-
+        checkIfRipe();
+        grow();
     }
 
-    public float getWater() {
-        return parentBranch.getWater();
+    /**
+     * Checks if the fruit is ripe. If the fruit is ripe it falls.
+     */
+    private void checkIfRipe() {
+        if (ripeness >= 1) {
+            fall();
+        }
+    }
+
+    /**
+     * Grows this fruit
+     */
+    private void grow() {
+        if (parentBranch.getGlucose(2) > 0) {
+            ripeness += ripeningSpeed;
+        }
+    }
+
+    /**
+     * @return float Returns the ripeness amount (between 0 and 1)
+     */
+    public float getRipeness() {
+        return ripeness;
     }
 }
--- src/main/java/simulation/tree/tree_organ/branch_component/subcomponents/FruitGraphics.java
+++ src/main/java/simulation/tree/tree_organ/branch_component/subcomponents/FruitGraphics.java
@@ -3,10 +3,15 @@ package simulation.tree.tree_organ.branch_component.subcomponents;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
+
+import simulation.Configuration;
+
 import java.awt.Color;
 
 public class FruitGraphics extends SubcomponentGraphics {
-    private final float size = 15;
+    private final float size = Configuration.fruitSize;
+
+    private Fruit fruit;
 
     /**
      * Constructor for creating FlowerGraphics object
@@ -15,6 +20,17 @@ public class FruitGraphics extends SubcomponentGraphics {
      */
     FruitGraphics(Fruit fruit) {
         super(fruit);
+        this.fruit = fruit;
+    }
+
+    Color getColor() {
+        float ripeness = fruit.getRipeness();
+        if (ripeness > 1)
+            ripeness = 1;
+
+        float h = (1.0f - ripeness) * 0.25f;
+        Color color = Color.getHSBColor(h, 0.6f, 0.7f);
+        return color;
     }
 
     /**
@@ -23,7 +39,7 @@ public class FruitGraphics extends SubcomponentGraphics {
     public void show(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
 
-        g2d.setColor(new Color(255, 0, 0, 150));
+        g2d.setColor(getColor());
 
         float y = getY();
         float x = direction * size;
